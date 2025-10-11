2bbbfbad8f6bafb51b62c9502a2ffaedf8272a92
johnmcphillips <john.mcphillips@gmail.com>
2025-10-11 11:28:24 -0500

    layer build

--- modules/lambda_get/app.py
+++ modules/lambda_get/app.py
@@ -1,5 +1,4 @@
-import json
-import requests
+import json, requests, boto3
 
 url = "https://api.wheretheiss.at/v1/satellites/25544"
 
@@ -11,9 +10,13 @@ def fetch_iss_location():
         "velocity": response.json().get("velocity")
     }
 
-def main():
+def lambda_handler(event, context):
     location = fetch_iss_location()
     print(json.dumps(location, indent=4))
+    return {
+        "statusCode": 200,
+        "body": json.dumps(location)
+    }
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    lambda_handler({}, {})
--- modules/main.tf
+++ modules/main.tf
@@ -6,18 +6,43 @@ resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
-data "archive_file" "lambda_zip" {
+resource "null_resource" "create_layer" {
+    provisioner "local-exec" {
+        interpreter = ["/bin/bash","-lc"]
+        command = <<EOF
+        set -euo pipefail
+        rm -rf ${path.module}/lambda_get/.build && rm -rf ${path.module}/lambda_get/python &&
+        mkdir -p ${path.module}/lambda_get/.build && mkdir -p ${path.module}/lambda_get/python &&
+        python3 -m pip install -r ${path.module}/lambda_get/requirements.txt -t ${path.module}/lambda_get/python
+        EOF
+    }
+}
+
+data "archive_file" "lambda_requirements" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_get"
+  output_path = "${path.module}/.build/requirements.zip"
+  depends_on = [null_resource.create_layer]
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_get/app.py"
   output_path = "${path.module}/.build/app.zip"
 }
 
+resource aws_lambda_layer_version "lambda_library_layer" {
+    filename          = data.archive_file.lambda_requirements.output_path
+    layer_name        = "lambda_library_layer"
+    compatible_runtimes = ["python3.12"]
+}
 resource "aws_lambda_function" "iss_tracker_lambda" {
   function_name = "ISS-Tracker-Lambda"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "main.handler"
-  runtime       = "python3.10"
+  handler       = "app.lambda_handler"
+  runtime       = "python3.12"
   filename      = data.archive_file.lambda_zip.output_path
+  layers        = [aws_lambda_layer_version.lambda_library_layer.arn]    
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
